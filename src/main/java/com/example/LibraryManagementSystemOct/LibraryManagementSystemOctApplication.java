29ab43b86dd25c60a8985529b8053e3e265da223
SujitJamdade <jamdadesujit58@gmail.com>
2023-10-18 01:00:14 +0530

    Project Initiated

---
+++ src/main/java/com/example/LibraryManagementSystemOct/LibraryManagementSystemOctApplication.java
@@ -0,0 +1,13 @@
+package com.example.LibraryManagementSystemOct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemOctApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagementSystemOctApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LibraryManagementSystemOct/LibraryManagementSystemOctApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryManagementSystemOct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemOctApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
